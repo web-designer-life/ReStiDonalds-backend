f51b3cb4f19a9295077204f332cc772d3ae201dd
Anton Yulbarysau <io57196657@gmail.com>
2021-05-26 18:23:52 +0300

    edit: products.js

--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,6 @@ const router = Router()
 const products = require('../public/products')
 
 router.get('/', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/json'
-    })
-
     res.end(JSON.stringify(products))
 })
 
