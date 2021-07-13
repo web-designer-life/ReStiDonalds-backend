b4f3d4ea3c34f9457de5769571b36506b3b085c6
Anton Yulbarysau <io57196657@gmail.com>
2021-07-13 11:16:56 +0300

    add: mongodb

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const menuRoutes = require('./routes/menu')
-const basketRoutes = require('./routes/basket')
+const addProductRoutes = require('./routes/addProduct')
 const productsRoutes = require('./routes/products')
 
 const app = express()
@@ -17,16 +18,32 @@ app.engine('hbs', hbs.engine) // registrate handlebars in express
 app.set('view engine', 'hbs') // use handlebars
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/menu', menuRoutes)
-app.use('/basket', basketRoutes)
+app.use('/addProduct', addProductRoutes)
 
 app.use('/api/products', productsRoutes)
 
-
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+    try {
+        const url = 'mongodb+srv://ReStiAdmin:aUnVoLXQSovIK3dN@cluster0.zyvpj.mongodb.net/ReStiDonalds'
+
+        await mongoose.connect(url, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useFindAndModify: false 
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
